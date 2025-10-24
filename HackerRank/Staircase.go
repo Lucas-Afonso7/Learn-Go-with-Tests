bdc6eb6cf6df702230ee0de92b01589331e2449e
Lucas-Afonso7 <lucasafonsoretiro7@gmail.com>
2025-10-24 10:27:01 -0300

    add code for solution hackerRank

--- HackerRank/Staircase.go
+++ HackerRank/Staircase.go
@@ -20,7 +20,7 @@ Constraints
 .
 Sample Input
 
-6 
+6
 
 Sample Output
 
@@ -35,3 +35,53 @@ Explanation
 The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
 */
 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int32) {
+    // Write your code here
+    for i := int32(1); i <= n; i++ {
+        spaces := strings.Repeat(" ", int(n-i))
+        hashes := strings.Repeat("#", int(i))
+        fmt.Println(spaces + hashes)
+    }
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
